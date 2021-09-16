428a4e38b63338a34558a15b39ef7b07f8fc6c2d
Tiago Moutinho <t.g.moutinho@gmail.com>
2021-09-17 00:06:29 +0100

    Improve Vector2 class

--- niceBoids/Vector2.h
+++ niceBoids/Vector2.h
@@ -7,24 +7,26 @@ class Vector2
 public:
 	Vector2() = default;
 	Vector2(float x, float y) : _x(x), _y(y) {};
-	Vector2(const Vector2& v2) { _x = v2.X(); _y = v2.Y(); };
 
-	inline float X() const { return _x; };
-	inline float Y() const { return _y; };
+	float X() const { return _x; };
+	float Y() const { return _y; };
 
-	inline bool operator!=(const Vector2& v2) { return (_x != v2.X() || _y != v2.Y()); };
-	inline bool operator==(const Vector2& v2) { return (_x == v2.X() && _y == v2.Y()); };
+	void setX(float newX) { _x = newX; };
+	void setY(float newY) { _y = newY; };
 
-	inline Vector2 operator+(const Vector2& v2) { return Vector2(_x + v2.X(), _y + v2.Y()); };
-	inline Vector2 operator-(const Vector2& v2) { return Vector2(_x - v2.X(), _y - v2.Y()); };
-	
-	inline Vector2 operator*(const float& scalar) { return Vector2(_x * scalar, _y * scalar); };
-	inline Vector2 operator/(const float& scalar) { return Vector2(_x / scalar, _y / scalar); };
+	bool operator!=(const Vector2& v2) const { return (_x != v2.X() || _y != v2.Y()); };
+	bool operator==(const Vector2& v2) const { return (_x == v2.X() && _y == v2.Y()); };
 
-	inline float length() { return std::sqrt(_x*_x + _y*_y); };
-	inline Vector2 normalize() { float len = this->length(); return Vector2(_x / len, _y / len); };
+	Vector2 operator+(const Vector2& v2) const { return Vector2(_x + v2.X(), _y + v2.Y()); };
+	Vector2 operator-(const Vector2& v2) const { return Vector2(_x - v2.X(), _y - v2.Y()); };
 
-	inline static float dot(const Vector2& v1, const Vector2& v2) { return v1.X() * v2.X() + v1.Y() * v2.Y(); };
+	Vector2 operator*(const float& scalar) const { return Vector2(_x * scalar, _y * scalar); };
+	Vector2 operator/(const float& scalar) const { return Vector2(_x / scalar, _y / scalar); };
+
+	float length() const { return std::sqrt(_x*_x + _y*_y); };
+	Vector2 normalize() const { float len = this->length(); return Vector2(_x / len, _y / len); };
+
+	static float dot(const Vector2& v1, const Vector2& v2) { return v1.X() * v2.X() + v1.Y() * v2.Y(); };
 
 private:
 	float _x;
